6041aca9c48cc7c378b70e3e804379f1a58760b2
pandasid.12345@outlook.com <pandasid.12345@outlook.com>
2025-11-12 13:24:35 +0000

    incremental model

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -1,3 +1,9 @@
 {% macro generate_schema_name(custom_schema_name, node) -%}
-{{ return(custom_schema_name) }}
+  {# Use provided schema if explicitly set #}
+  {% if custom_schema_name is not none %}
+      {{ return(custom_schema_name) }}
+  {% else %}
+      {# Fallback to the target.schema (from your active dbt connection) #}
+      {{ return(target.schema) }}
+  {% endif %}
 {%- endmacro %}
